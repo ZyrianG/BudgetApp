50e7b9df2a17d1d089fdbfdfe6c9370231230a92
ZyrianG <g.zyrian@gmail.com>
2020-08-13 23:52:53 -0400

    Added custom colors

---
+++ budget-app/tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  purge: [],
+  theme: {
+    extend: {
+      colors: {
+        sand: {
+          default: '#e1b382',
+          shadow: '#c89666',
+        },
+        night: {
+          default: '#2d545e',
+          shadow: '#12343b',
+        }
+      },
+    }
+  },
+  variants: {},
+  plugins: [],
+}
