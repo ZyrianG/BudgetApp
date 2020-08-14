b56b4c3afbafeb857a2aabce618e0b4d09c9575b
ZyrianG <g.zyrian@gmail.com>
2020-08-13 23:53:51 -0400

    Update app.js with header/sidebar path

--- budget-app/src/App.js
+++ budget-app/src/App.js
@@ -1,15 +1,21 @@
 import React from 'react';
-import { Header } from './components/Header';
+import { Header } from './components/Header/Header';
+import { Sidebar } from './components/Sidebar/Sidebar';
 import { Expenses } from './components/Expenses';
 import { Balance } from './components/Balance';
 
 function App() {
   return (
-    <div className="App">
-      <Header/>
-      <div className="container">
-          <Balance/>
-          <Expenses/>
+    <div className="App flex mb-4 h-screen">
+      <div className="side w-1/12">
+        <Sidebar />
+      </div>
+      <div className="page-container w-11/12">
+        <Header/>
+        <div className="container">
+            <Balance/>
+            <Expenses/>
+        </div>
       </div>
     </div>
   );
