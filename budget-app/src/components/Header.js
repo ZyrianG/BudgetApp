2b8b4b21f105edfaf0b5fd0968b2de7a8ff569ba
ZyrianG <g.zyrian@gmail.com>
2020-08-13 23:53:33 -0400

    Created sidebar component Updated header component directory

--- budget-app/src/components/Header.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export const Header = () => {
-    return (
-    <header className="App-header">
-        <h2 className="text-6xl text-orange-600">Header</h2>
-    </header>
-    )
-}
---
+++ budget-app/src/components/Sidebar/Sidebar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Sidebar = () => {
+    return (
+        <div className="flex bg-night items-start justify-between flex-wrap h-full">
+            <div className="block items-center text-sand-shadow">
+                <svg class="h-8 w-8 m-4" width="54" height="54" viewBox='0 0 10 8'><path d='M1 1h8M1 4h 8M1 7h8' stroke='#000' stroke-width='2' stroke-linecap='round'/></svg>
+            </div>
+        </div>
+    )
+}
